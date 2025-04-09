f91d4a6e9563b7bc43b73f25b6a3b29551377adb
Martin <martinmitchell75@gmail.com>
2025-04-09 07:22:22 +0100

    added rds linked role to iam.tf

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -115,6 +115,7 @@ data "aws_iam_policy_document" "ec2" {
       "ec2:DeleteRouteTable",
       "ec2:DeleteVpcEndpoints",
       "ec2:DisassociateRouteTable",
+      "ec2:DescribeAvailabilityZones",
       "ec2:DeleteRoute",
       "ec2:DescribePrefixLists",
       "ec2:DescribeSubnets",
@@ -166,6 +167,7 @@ data "aws_iam_policy_document" "rds" {
       "rds:DeleteDBInstance",
       "rds:ListTagsForResource",
       "rds:AddTagsToResource",
+      "rds:ModifyDBSubnetGroup",
       "rds:ModifyDBInstance"
     ]
     resources = ["*"]
@@ -181,4 +183,28 @@ resource "aws_iam_policy" "rds" {
 resource "aws_iam_user_policy_attachment" "rds" {
   user       = aws_iam_user.cd.name
   policy_arn = aws_iam_policy.rds.arn
+}
+
+data "aws_iam_policy_document" "service_linked_rds" {
+  statement {
+    effect    = "Allow"
+    actions   = ["iam:CreateServiceLinkedRole"]
+    resources = ["arn:aws:iam::*:role/aws-service-role/rds.amazonaws.com/AWSServiceRoleForRDS"]
+    condition {
+      test     = "StringLike"
+      variable = "iam:AWSServiceName"
+      values   = ["rds.amazonaws.com"]
+    }
+  }
+}
+ 
+resource "aws_iam_policy" "service_linked_rds" {
+  name        = "${aws_iam_user.cd.name}-service_linked_rds"
+  description = "Allow Amazon RDS to call AWS services on behalf of your DB instances."
+  policy      = data.aws_iam_policy_document.service_linked_rds.json
+}
+ 
+resource "aws_iam_user_policy_attachment" "service_linked_rds" {
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.service_linked_rds.arn
 }
